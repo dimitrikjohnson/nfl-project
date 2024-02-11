03ecbe914efd444b0d18bd5c3c39b1dce7cf88da
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-02-10 19:18:46 -0500

    complete initial layout

--- app/components/Instructions.tsx
+++ app/components/Instructions.tsx
@@ -3,14 +3,23 @@ import NFLLogo from "../images/nflLogo.svg";
 
 export default function Instructions() {
     return (
-        <section className="w-full flex mb-14">
-            <Image
-                src={NFLLogo}
-                width={150}
-                alt="NFL Logo"
-                priority
-            />
-            <p className="font-rubik">Select a team's card to view their stats for the current season.</p>
+        <section className="w-full grid md:flex gap-12 mb-20">
+            <div className="flex justify-center md:block">
+                <Image
+                    src={NFLLogo}
+                    width={175}
+                    alt="NFL Logo"
+                    priority
+                /> 
+            </div>
+            
+            <div>
+                <h1 className="font-protest text-5xl uppercase mb-6">Select a team</h1>
+                <p className="font-rubik text-lg">
+                    Select a team's card to view their stats for the current season. If the chosen team is in the offseason, the team's stats
+                    from the previous season will be displayed.
+                </p>
+            </div>
         </section>
     );
 }
