383ece43607aca6d8975bcda6f507cf9326ad68f
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-02-10 19:20:53 -0500

    pass selected team ID from team list to parent page

--- app/components/ListOfTeams.tsx
+++ app/components/ListOfTeams.tsx
@@ -2,8 +2,8 @@ import 'client-only';
 import { useState, useEffect } from 'react';
 import { getAllTeams } from "../data/API";
 
-export default function ListOfTeams() {
-    const [teams, setTeams] = useState([]);
+export default function ListOfTeams({ childToParent }) {
+    const [teams, setTeams] = useState([])
     
     const getTeams = () => getAllTeams().then(
         (res) => {
@@ -23,11 +23,11 @@ export default function ListOfTeams() {
             </div>
             <div className="grid gap-7 grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8">
                 { teams.map((x) =>
-                    <div key={ x.team.id } className="p-1.5 rounded-lg transition ease-in-out delay-50 hover:-translate-y-1 sm:hover:scale-90 md:hover:scale-110 duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none" style={{ backgroundColor: "#" + x.team.color }}>
+                    <div onClick={ () => childToParent(x.team.id) } key={ x.team.id } className="p-1.5 rounded-lg transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-90 md:hover:scale-110 duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none" style={{ backgroundColor: "#" + x.team.color }}>
                         <div>
                             <img src={ x.team.shortDisplayName == 'Giants' ? x.team.logos[1].href : x.team.logos[0].href } alt={ x.team.displayName + " logo" } />
                         </div>
-                        <p className="font-rubik text-lg font-semibold text-center" style={{ color: "#" + x.team.alternateColor }}>{ x.team.displayName }</p>
+                        <p className="font-rubik text-xl font-semibold text-center" style={{ color: "#" + x.team.alternateColor }}>{ x.team.displayName }</p>
                     </div>
                 )}
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,23 @@ import Image from "next/image";
 import NavBar from "./components/NavBar";
 import ListOfTeams from "./components/ListOfTeams";
 import Instructions from "./components/Instructions";
+import SelectedTeam from "./components/SelectedTeam";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedTeam, setSelectedTeam] = useState("");
+  
+  const childToParent = (childData) => {
+    setSelectedTeam(childData)
+    console.log(selectedTeam)
+  }
+  
   return (
     <>
       <NavBar />
       <main className="flex min-h-screen flex-col items-center justify-between px-11">
-        <Instructions />
-        <ListOfTeams />
+        { selectedTeam ? <SelectedTeam /> : <Instructions />}
+        <ListOfTeams childToParent={childToParent} />
       </main>
     </>
   );
