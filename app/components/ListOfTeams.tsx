2a4224745af63a48415769860a6f22256e9ee4bc
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-02-14 22:29:26 -0500

    identify Super Bowl winner

--- app/components/ListOfTeams.tsx
+++ app/components/ListOfTeams.tsx
@@ -1,19 +1,32 @@
 import 'client-only';
 import { useState, useEffect } from 'react';
-import { getAllTeams } from "../data/API";
+import { getAllTeams, getSuperBowlWinner } from "../data/API";
 
 export default function ListOfTeams({ childToParent }) {
     const [teams, setTeams] = useState([])
+    const [sbWinner, setSBWinner] = useState({})
     
     const getTeams = () => getAllTeams().then(
         (res) => {
             setTeams(res)
         }
     )
-    
+
+    const getSBWinner = () => getSuperBowlWinner().then(
+        (res) => {
+            if (res) {
+                setSBWinner({
+                    headline: res.headline, 
+                    winner: res.winnerID
+                })
+            }
+        }
+    )
+
     // run getTeams() when the page loads
     useEffect(() => {
         getTeams()
+        getSBWinner()
     }, [])
 
     return (
@@ -25,11 +38,13 @@ export default function ListOfTeams({ childToParent }) {
                 { teams.map((x) =>
                     <a
                     href="#top-of-page" 
-                    onClick={ () => childToParent(x.team.id) } 
+                    onClick={ () => childToParent(x.team.id) }
+                    aria-selected={ sbWinner.winner == x.team.id }
                     key={ x.team.id } 
-                    className="p-1.5 rounded-md transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-90 md:hover:scale-110 duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none" 
+                    className="p-1.5 rounded-md transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-90 md:hover:scale-110 aria-selected:border-2 aria-selected:border-gold duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none" 
                     style={{ backgroundColor: "#" + x.team.color }}
                     >
+                        { sbWinner.winner == x.team.id ? <p className="font-rubik text-gold text-center">{ sbWinner.headline } Champions</p> : null }
                         <div>
                             <img src={ x.team.shortDisplayName == 'Giants' ? x.team.logos[1].href : x.team.logos[0].href } alt={ x.team.displayName + " logo" } />
                         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
       },
       colors: {
         secondaryGrey: "#848484",
+        gold: "#FFD700",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
