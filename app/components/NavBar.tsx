1470b1bceea3d220220622ef4c4d999ad467acde
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-04-07 15:27:52 -0400

    adjust spacing

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,5 @@
 export default function NavBar() {
     return (
-        <nav className="w-full py-6 px-11 sticky top-0 mb-14">NFL temp navbar</nav>
+        <nav className="w-full py-6 px-7 md:px-11 top-0 mb-14">NFL temp navbar</nav>
     );
 }
