e340ef00d62c6bd28ccfa39779faa869203e4b69
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-04-07 15:25:58 -0400

    remove unneeded comments

--- app/apiCalls/getTeamScheduleDetailed.tsx
+++ app/apiCalls/getTeamScheduleDetailed.tsx
@@ -1,25 +1,22 @@
-async function getTeamScheduleDetailed( teamID, seasonYear ) {
+import fetchCurrentSeason from "./fetchCurrentSeason";
+
+async function getTeamScheduleDetailed( teamID ) {
     let output = []
+    const currentSeason = await fetchCurrentSeason()
 
     // fetch to get the team's bye week (the bye week in the team schedule API is incorrect for some teams)
-    const res = await fetch("https://fantasy.espn.com/apis/v3/games/ffl/seasons/" + seasonYear + "?view=proTeamSchedules_wl", { method: "get" })
+    const res = await fetch("https://fantasy.espn.com/apis/v3/games/ffl/seasons/" + currentSeason + "?view=proTeamSchedules_wl", { method: "get" })
     if (!res.ok) 
         throw new Error('Failed to fetch team bye week data') 
     
     const data = await res.json()
-    //var chosenTeam, byeWeek
 
-    //try {
-        const chosenTeam = data[0].settings.proTeams.find(team => team.id == teamID)
-        const byeWeek = chosenTeam.byeWeek
-    //}
-    //catch (TypeError) {
-        // "settings" and "byeWeek" will be undefined after choosing multiple teams; seemingly has no effect on website
-    //}
+    const chosenTeam = data.settings.proTeams.find(team => team.id == teamID)
+    const byeWeek = chosenTeam.byeWeek
 
     // fetch the post, regular, and preseason schedules for the chosen team
     for(let counter = 3; counter >= 1; counter -= 1) {
-        const res1 = await fetch("https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/" + teamID + "/schedule?season=" + seasonYear + "&seasontype=" + counter, { method: "GET" })
+        const res1 = await fetch("https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/" + teamID + "/schedule?season=" + currentSeason + "&seasontype=" + counter, { method: "GET" })
         
         if (!res1.ok) 
             throw new Error('Failed to fetch team season data') 
