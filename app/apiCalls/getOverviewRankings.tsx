dc10863027e430dad4d94143696d7d9450467343
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-04-21 22:14:02 -0400

    shorten labels for spacing

--- app/apiCalls/getOverviewRankings.tsx
+++ app/apiCalls/getOverviewRankings.tsx
@@ -1,25 +1,34 @@
 import fetchCurrentSeason from "./fetchCurrentSeason";
 
 export default async function getOverviewRankings( teamID ) {
-    const currentSeason = await fetchCurrentSeason()
+    const currentSeason = await fetchCurrentSeason();
 
-    const res = await fetch("https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/" + currentSeason + "/types/2/teams/" + teamID + "/statistics", { method: "get" })
-    const data = await res.json()
+    const res = await fetch(`https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/${currentSeason}/types/2/teams/${teamID}/statistics`, { method: "get" })
+    const data = await res.json();
 
     if (!res.ok) {
         // This will activate the closest `error.js` Error Boundary
-        throw new Error('Failed to fetch team stats')
+        throw new Error('Failed to fetch team stats');
     }
 
-    const categories = data.splits.categories
+    const categories = data.splits.categories;
 
     return {
-        ppg: categories[1].stats[30],
-        ypg: categories[1].stats[39],
-        passingYPG: categories[1].stats[22],
-        rushingYPG: categories[2].stats[13],
-        sacks: categories[4].stats[14],
-        ints: categories[5].stats[0],
-        takeaways: categories[10].stats[20]
+        ppg: getValues(categories[1].stats[30], "Points P/G"),
+        ypg: getValues(categories[1].stats[39], "Yards P/G"),
+        passingYPG: getValues(categories[1].stats[22], "Pass Yards P/G"),
+        rushingYPG: getValues(categories[2].stats[13], "Rush Yards P/G"),
+        sacks: getValues(categories[4].stats[14]),
+        ints: getValues(categories[5].stats[0]),
+        takeaways: getValues(categories[10].stats[20])
+    }
+}
+
+function getValues(category, shortName = category.displayName) {
+    return {
+        shortName: shortName,
+        longName: category.displayName,
+        rank: category.rank,
+        rankDisplayValue: category.rankDisplayValue
     }
 }
--- app/components/SelectedTeamTabs/OverviewComponents/TeamRankings.tsx
+++ app/components/SelectedTeamTabs/OverviewComponents/TeamRankings.tsx
@@ -4,8 +4,8 @@ import getOverviewRankings from '@/app/apiCalls/getOverviewRankings';
 import getRankColor from '../../helpers/getRankColor';
 
 export default function TeamRankings({ teamID }) {
-    const [offensiveRanks, setOffensiveRanks] = useState({})
-    const [defensiveRanks, setDefensiveRanks] = useState({})
+    const [offensiveRanks, setOffensiveRanks] = useState({});
+    const [defensiveRanks, setDefensiveRanks] = useState({});
 
     const getTeamRankings = () => getOverviewRankings(teamID).then(
         (res) => {
@@ -14,12 +14,12 @@ export default function TeamRankings({ teamID }) {
                 ypg: res.ypg,
                 passingYPG: res.passingYPG,
                 rushingYPG: res.rushingYPG
-            })
+            });
             setDefensiveRanks({
                 sacks: res.sacks,
                 ints: res.ints,
                 takeaways: res.takeaways
-            })
+            });
         }
     )
     
@@ -27,8 +27,8 @@ export default function TeamRankings({ teamID }) {
         var output = []
         for (var rank in ranks) {
             output.push(
-                <div key={ ranks[rank].name }>
-                    <p className="pb-2">{ ranks[rank].displayName }</p>
+                <div key={ ranks[rank].longName } className="grid grid-rows-2 gap-2 content-between">
+                    <p>{ ranks[rank].shortName }</p>
                     <p className={ getRankColor(ranks[rank].rank, false) }>{ ranks[rank].rankDisplayValue }</p>
                 </div>
             )  
@@ -42,16 +42,16 @@ export default function TeamRankings({ teamID }) {
 
     return (
         <>
-            <div className="grid grid-cols-1 xl:grid-cols-2 gap-6 mb-10">
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-4 mb-10">
                 <div>
                     <h3 className="font-protest text-2xl 2xl:text-3xl pb-2">Offensive Rankings</h3>
-                    <div className="font-rubik grid grid-cols-2 md:grid-cols-4 gap-3 bg-sectionColor p-3 rounded-md">
+                    <div className="font-rubik grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-2 bg-sectionColor p-3 rounded-md">
                         { displayTeamRankings(offensiveRanks) }
                     </div>
                 </div>
                 <div>
                     <h3 className="font-protest text-2xl 2xl:text-3xl pb-2">Defensive Rankings</h3>
-                    <div className="font-rubik grid grid-cols-2 md:grid-cols-3 gap-3 bg-sectionColor p-3 rounded-md">
+                    <div className="font-rubik grid grid-cols-2 md:grid-cols-3 gap-3 md:gap-2 bg-sectionColor p-3 rounded-md">
                         { displayTeamRankings(defensiveRanks) }
                     </div>
                 </div>
