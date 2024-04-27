588652642dc9f1474f99f531c9f59b6c5f68441d
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-04-27 00:30:14 -0400

    fix broken fetch request

--- app/apiCalls/getTeamScheduleDetailed.tsx
+++ app/apiCalls/getTeamScheduleDetailed.tsx
@@ -1,41 +1,42 @@
-import fetchCurrentSeason from "./fetchCurrentSeason";
-
 async function getTeamScheduleDetailed( teamID ) {
-    let output = []
-    const currentSeason = await fetchCurrentSeason()
-
-    // fetch to get the team's bye week (the bye week in the team schedule API is incorrect for some teams)
-    const res = await fetch("https://fantasy.espn.com/apis/v3/games/ffl/seasons/" + currentSeason + "?view=proTeamSchedules_wl", { method: "get" })
-    if (!res.ok) 
-        throw new Error('Failed to fetch team bye week data') 
-    
-    const data = await res.json()
-
-    const chosenTeam = data.settings.proTeams.find(team => team.id == teamID)
-    const byeWeek = chosenTeam.byeWeek
+    let output = [];
 
     // fetch the post, regular, and preseason schedules for the chosen team
     for(let counter = 3; counter >= 1; counter -= 1) {
-        const res1 = await fetch("https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/" + teamID + "/schedule?season=" + currentSeason + "&seasontype=" + counter, { method: "GET" })
+        const res1 = await fetch(`https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/${teamID}/schedule?seasontype=${counter}`, { method: "GET" });
         
-        if (!res1.ok) 
-            throw new Error('Failed to fetch team season data') 
+        if (!res1.ok) throw new Error('Failed to fetch team season data');
         
-        const data1 = await res1.json()
+        const schedule = await res1.json();
 
         // skip empty arrays (occurs for teams that didn't make the playoffs)
-        if (data1.requestedSeason) {
-            if (data1.requestedSeason.type == "2") {
-                data1.events.splice(byeWeek-1, 0, { week: { number: byeWeek }})
-                output.push({ requestedSeason: data1.requestedSeason.name, games: data1.events, byeWeek: byeWeek })
+        if (schedule.requestedSeason) {
+            if (schedule.requestedSeason.type == "2") {
+                let weeks = [];
+                
+                for (const game of schedule.events) {
+                    weeks.push(game.week.number)
+                }
+                const byeWeek = findByeWeek(weeks, weeks.length);
+                
+                schedule.events.splice(byeWeek-1, 0, { week: { number: byeWeek }});
+                output.push({ requestedSeason: schedule.requestedSeason.name, games: schedule.events, byeWeek: byeWeek });
             }
+            // the pre and post seasons don't need the byeWeek value
             else {
-                output.push({ requestedSeason: data1.requestedSeason.name, games: data1.events, byeWeek: null })
+                output.push({ requestedSeason: schedule.requestedSeason.name, games: schedule.events, byeWeek: null });
             }
         }
     }
     
-    return output
+    return output;
 }
 
-export default getTeamScheduleDetailed
+// determine the bye week by finding the missing week in the schedule
+function findByeWeek(weeksArray, length) {
+    let result = Math.floor((length + 1) * (length + 2) / 2);
+    for (let i = 0; i < length; i++) result -= weeksArray[i];
+    return result;
+}
+
+export default getTeamScheduleDetailed;
