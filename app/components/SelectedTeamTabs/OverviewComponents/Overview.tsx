b2df5876605e83a087c3734356d9e6cffcb93565
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-05-05 01:01:37 -0400

    move Overview file to OverviewComponents

--- app/components/SelectedTeamTabs/OverviewComponents/Overview.tsx
+++ app/components/SelectedTeamTabs/OverviewComponents/Overview.tsx
@@ -1,38 +1,38 @@
 import 'client-only';
 import { useState, useEffect } from 'react';
 import fetchCurrentSeason from "@/app/apiCalls/fetchCurrentSeason";
-import getTeam from '@/app/apiCalls/getTeam';
-import TeamLeaders from './OverviewComponents/TeamLeaders';
-import TeamRankings from './OverviewComponents/TeamRankings';
-import LastTwoGames from './OverviewComponents/LastTwoGames';
+//import getTeam from '@/app/apiCalls/getTeam';
+import TeamLeaders from './TeamLeaders';
+import TeamRankings from './TeamRankings';
+import LastTwoGames from './LastTwoGames';
 
 export default function Overview({ teamID }) {
     const [currentSeason, setCurrentSeason] = useState("")
-    const [teamAltColor, setTeamAltColor] = useState("")
+    //const [teamAltColor, setTeamAltColor] = useState("")
 
     const getCurrentSeason = () => fetchCurrentSeason().then(
         (res) => setCurrentSeason(res)
     )
-    
+    /*
     const getTeamAltColor = () => getTeam({teamID}).then(
         (res) => setTeamAltColor(res.alternateColor)
     )
-    
+    */
     // only run getCurrentSeason() on the first render
     useEffect(() => {
         getCurrentSeason()
-    }, [])
-
+    }, []);
+    /*
     useEffect(() => {
         getTeamAltColor()
     }, [teamID])
-
+    */
     return (
         <>
-            <h2 className="font-protest text-3xl 2xl:text-4xl uppercase pb-2 mb-9 border-b-2">{ currentSeason } season overview</h2>
-            <TeamLeaders teamID={ teamID } teamAltColor={ teamAltColor }/>
+            <h2 className="font-protest text-3xl 2xl:text-4xl uppercase pb-2 mb-9 border-b-2">{ currentSeason } overview</h2>
+            <TeamLeaders teamID={ teamID } />
             <TeamRankings teamID={ teamID } />
-            <div className="grid grid-cols-1 2xl:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
                 <LastTwoGames teamID={ teamID }/>
                 <div>
                     <h3 className="font-protest text-2xl 2xl:text-3xl pb-2">Next Two Games</h3>
