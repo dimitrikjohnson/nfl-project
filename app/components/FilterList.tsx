e41d056d9685c0e8ffee7f984f1b93eb27e0c351
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-05-09 23:15:51 -0400

    redesign roster with filter list component

---
+++ app/components/FilterList.tsx
@@ -0,0 +1,29 @@
+import 'client-only';
+import allTeamsColors from './data/allTeamsColors.json';
+
+export default function FilterList({ tags, teamID, isMobile, showTeamColors, filter, childToParent }) {
+    const handleFilterPick = (tag) => {
+        childToParent(tag);
+    }
+
+    return (
+        <div className={ isMobile ? "flex w-full overflow-x-auto mb-4 md:hidden" : "hidden md:flex" }>
+            { tags.map(tag =>
+                <button key={ tag } 
+                className="mr-2.5 capitalize border text-center text-nowrap border-secondaryGrey/[.50] hover:bg-secondaryGrey/[0.25] py-2 md:py-0 px-3.5 rounded-md last-of-type:m-0"
+                style={ showTeamColors && (tag == filter) 
+                        ? { 
+                            backgroundColor: allTeamsColors[teamID].bgColor, 
+                            color: allTeamsColors[teamID].textColor, 
+                            border: `1px solid ${allTeamsColors[teamID].textColor}` 
+                          } 
+                        : null 
+                }
+                onClick={ () => handleFilterPick(tag) } 
+                >
+                    { tag }
+                </button>
+            )}
+        </div>
+    )
+}
--- app/components/SelectedTeamTabs/Roster.tsx
+++ app/components/SelectedTeamTabs/Roster.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import DefaultHeadshot from '../../images/default_headshot.png';
 import getRoster from '@/app/apiCalls/getRoster';
 import getTeam from '@/app/apiCalls/getTeam';
-import allTeamsColors from '../data/allTeamsColors.json';
+import FilterList from '../FilterList';
 
 export default function Roster({ teamID }) {
     const [teamAltColor, setTeamAltColor] = useState("");
@@ -13,134 +13,127 @@ export default function Roster({ teamID }) {
     const [spinner, setSpinner] = useState(false);
     const [filter, setFilter] = useState("all");
     const [popupActive, setPopupActive] = useState(false);
-    const [colors, setColors] = useState({});
 
     /*
      * the tags match the ones assigned to the positions
      * they allow the user to filter the roster results
     */
-    const tags = ["all", "fantasy", "offense", "defense", "special teams"];
+    const tags = ["all", "offense", "defense", "special teams", "fantasy"];
 
-    // establish the styling for the labels
-    const labelClasslist = "uppercase text-lighterSecondaryGrey mr-2.5";
+    const tableHeadings = ["Name", "Age", "Height", "Weight", "College", "Experience"];
+
+    // establish the styling for the table cells
+    const tableCellClasses = "text-start p-1.5 md:px-3";
     
-    const getTeamRoster = () => {
-        getRoster(teamID).then((res) => {
+    const getTeamRoster = () => getRoster(teamID).then(
+        (res) => {
             setSpinner(false);
             setRoster(res);
-        });
-    }
+        }
+    );
     
     const getTeamAltColor = () => getTeam({teamID}).then(
         (res) => setTeamAltColor(res.alternateColor)
     );
 
+    const childToParent = (dataFromChild) => {
+        setFilter(dataFromChild);
+    }
+    
     const displayPositions = () => {
-        return ( <>
-            { Object.keys(roster).map(position =>
-                <div key={ position } className="mb-8 last-of-type:mb-0">
-                    { roster[position].tags.includes(filter) 
-                      ? <>
-                            <h3 className="font-protest text-2xl 2xl:text-3xl pb-2">
-                                { position }{ Object.keys(roster[position].players).length > 1 ? "s" : null }
-                            </h3>
-                            <div className="font-rubik grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-6">
-                                { displayPlayerCards(roster[position].players) }
-                            </div>
-                        </>
-                      : null
-                    } 
-                </div>
-            )}
+        return (<>
+            <div className="mt-8 md:mt-0 overflow-x-auto sm:overflow-visible">
+                <table className="table-auto relative w-full text-nowrap font-rubik bg-sectionColor rounded-md">
+                    <thead className="border-b border-secondaryGrey text-right sticky top-0 z-20">
+                        <tr>
+                            { tableHeadings.map(heading =>
+                                <th key={ heading } className="py-2.5 px-1.5 md:px-3 text-start bg-sectionColor first-of-type:rounded-tl-md last-of-type:rounded-tr-md">{ heading }</th>
+                            )}    
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { Object.keys(roster).map(position =>
+                            roster[position].tags.includes(filter) &&
+                            <>
+                                <tr key={ position } className="bg-[#282e37] border-b border-secondaryGrey/[.50]">
+                                    <th colSpan={ tableHeadings.length } className="text-start py-2 px-2 md:px-3">
+                                        { position }{ Object.keys(roster[position].players).length > 1 ? "s" : null }
+                                    </th>
+                                </tr>
+                                { displayPlayerRows(roster[position].players) }
+                            </>
+                        )}
+                    </tbody>
+                </table>
+            </div>
         </>)
     }
+    
+    // determine the suffix for the player's position on the depth chart & seasons in league
+    const getOrdinal = (num) => {
+        let ord = 'th';
 
-    const displayPlayerCards = (position) => {
-        // determine the suffix for the player's position on the depth chart
-        const getOrdinal = (num) => {
-            let ord = 'th';
+        if (num % 10 == 1 && num % 100 != 11) { ord = 'st'; }
+        else if (num % 10 == 2 && num % 100 != 12) { ord = 'nd'; }
+        else if (num % 10 == 3 && num % 100 != 13) { ord = 'rd'; }
 
-            if (num % 10 == 1 && num % 100 != 11) { ord = 'st'; }
-            else if (num % 10 == 2 && num % 100 != 12) { ord = 'nd'; }
-            else if (num % 10 == 3 && num % 100 != 13) { ord = 'rd'; }
-
-            return ord;
-        }
+        return ord;
+    }
+    
+    const displayPlayerRows = (position) => {
         return ( <>
             { Object.keys(position).map(player =>
-                <div key={ position[player].playerValues.name } className="bg-sectionColor p-3 rounded-md">
-                    <div className="pb-1 mb-3 border-b flex gap-2">
-                        <p className="font-semibold">{ position[player].playerValues.name }</p>
-                        <p className="flex gap-1.5 text-lighterSecondaryGrey">
-                            { position[player].playerValues.jersey 
-                              ? <>
-                                    <span>#{ position[player].playerValues.jersey }</span>
-                                    <span>&#183;</span>
-                                </>
-                              : null 
-                            }                                 
-                            <span>
-                                { player == "1" 
-                                  ? "Starter"
-                                  : player + getOrdinal(Number(player)) + " string"
-                                }
-                            </span>
-                        </p>
-                    </div>
-                    <div className="flex gap-3">
-                        <div className="w-28 sm:w-32 md:w-40 relative rounded-sm shrink-0" style={{ background: '#' + teamAltColor }}>
+                <tr key={ player } className="border-b border-secondaryGrey/[.50] last-of-type:border-0">
+                    <td className={ "flex gap-1.5 md:gap-2.5 " + tableCellClasses }>
+                        <div className="w-14 relative rounded-sm shrink-0 z-10" style={{ background: '#' + teamAltColor }}>
                             { position[player].playerValues.headshot
-                              ? <img className="absolute bottom-0" src={ position[player].playerValues.headshot } alt={ position[player].playerValues.name } />
-                              : <Image className="w-28 sm:w-32 md:w-40 object-cover" src={ DefaultHeadshot }  alt="Default profile picture" priority />
-                            }
+                              ? <img className="relative bottom-0" src={ position[player].playerValues.headshot } alt={ position[player].playerValues.name } />
+                              : <Image className="w-14 object-cover rounded-sm" src={ DefaultHeadshot }  alt="Default profile picture" priority />
+                            } 
                         </div>
-                        <div>
-                            <p className="pb-1 md:pb-1.5">
-                                <span className={ labelClasslist }>Age:</span>
-                                <span>{ position[player].playerValues.age ? position[player].playerValues.age : "N/A" }</span>
-                            </p>
-                            <p className="pb-1 md:pb-1.5">
-                                <span className={ labelClasslist }>HT/WT:</span>
-                                <span>{ position[player].playerValues.height }, { position[player].playerValues.weight }</span>
-                            </p>
-                            <p className="pb-1 md:pb-1.5">
-                                <span className={ labelClasslist }>College:</span>
-                                <span>{ position[player].playerValues.college ? position[player].playerValues.college : "N/A" }</span>
-                            </p>
-                            <p>
-                                <span className={ labelClasslist }>Experience:</span>
+                        <div className="flex gap-1.5 m-auto mx-0">
+                            <p>{ position[player].playerValues.name }</p>
+                            <p className="flex gap-1.5 text-lighterSecondaryGrey text-sm items-end">
+                                { position[player].playerValues.jersey 
+                                  ? <>
+                                        <span>#{ position[player].playerValues.jersey }</span>
+                                        <span>&#183;</span>
+                                    </>
+                                  : null 
+                                }                                 
                                 <span>
-                                    { position[player].playerValues.experience == 0
-                                      ? "Rookie"
-                                      : position[player].playerValues.experience + getOrdinal(position[player].playerValues.experience) + " season" 
+                                    { player == "1" 
+                                      ? "Starter"
+                                      : player + getOrdinal(Number(player)) + " string"
                                     }
                                 </span>
                             </p>
                         </div>
-                    </div>
-                </div>
+                    </td>
+                    <td className={ tableCellClasses }>{ position[player].playerValues.age ? position[player].playerValues.age : "N/A" }</td>
+                    <td className={ tableCellClasses }>{ position[player].playerValues.height }</td>
+                    <td className={ tableCellClasses }>{ position[player].playerValues.weight }</td>
+                    <td className={ tableCellClasses }>{ position[player].playerValues.college ? position[player].playerValues.college : "Unknown" }</td>
+                    <td className={ tableCellClasses }>
+                        { position[player].playerValues.experience == 0
+                          ? "Rookie"
+                          : <span>{ position[player].playerValues.experience + getOrdinal(position[player].playerValues.experience) } season</span>
+                        }
+                    </td>
+                </tr>
             )}
         </>)
     }
-
-    const handleFilterPick = (tag) => {
-        setFilter(tag);
-        setPopupActive(false);
-    }
-
+ 
     useEffect(() => {
         setSpinner(true),
-        setColors({
-            bg: allTeamsColors[teamID].bgColor,
-            text: allTeamsColors[teamID].textColor
-        }),
         getTeamRoster(),
         getTeamAltColor()
     }, [teamID]);
 
     return (
         <>
-            <div className="flex justify-between pb-2 mb-9 border-b-2">
+            <div className="flex justify-between pb-2 mb-4 md:mb-9 border-b-2">
                 <h2 className="font-protest text-3xl 2xl:text-4xl uppercase">Current Roster</h2>
                 <div className="font-rubik flex">
                     <button 
@@ -149,32 +142,17 @@ export default function Roster({ teamID }) {
                     >
                         Filters
                     </button>
-                    <div className={ popupActive ? "absolute mt-10 w-36 right-0 mr-4 bg-stone-900 rounded-md border border-secondaryGrey/[.50]" : "hidden md:flex" }>
-                        { tags.map(tag =>
-                            <button key={ tag } 
-                            aria-selected={ tag == filter } 
-                            className="w-full md:w-auto md:mr-2.5 capitalize text-sm md:text-base md:border text-left md:text-center border-secondaryGrey/[.50] hover:bg-secondaryGrey/[0.25] py-1.5 md:py-0 px-3.5 rounded-md last-of-type:m-0"
-                            style={ tag == filter 
-                                    ? { 
-                                        backgroundColor: colors.bg, 
-                                        color: colors.text, 
-                                        border: `1px solid ${ colors.text }` 
-                                    } 
-                                    : null 
-                            }
-                            onClick={ () => handleFilterPick(tag) } 
-                            >
-                                { tag }
-                            </button>
-                        )}
-                    </div>
+                    <FilterList tags={ tags } teamID={ teamID } isMobile={ false } showTeamColors={ true } filter={ filter } childToParent={ childToParent }/>
                 </div>
             </div>    
             { spinner 
               ? <div className="w-full flex justify-center mt-5">
                     <ReactLoading type="spin" height={100} width={75} />
-                </div> 
-              : displayPositions()
+                </div>
+              : <>  
+                    { popupActive && <FilterList tags={ tags } teamID={ teamID } isMobile={ true } showTeamColors={ true } filter={ filter } childToParent={ childToParent }/> } 
+                    { displayPositions() }
+                </>
             }
         </>
     )
