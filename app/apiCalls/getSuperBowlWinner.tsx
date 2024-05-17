168655aafd05b4b62264eca5f2cd60db811c8a94
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-05-16 21:29:04 -0400

    fix Super Bowl winner and record error

--- app/apiCalls/getSuperBowlWinner.tsx
+++ app/apiCalls/getSuperBowlWinner.tsx
@@ -1,13 +1,20 @@
 async function getSuperBowlWinner() {
-    const res = await fetch("https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events", { method: "get" })
-    const data = await res.json()
-    const mostRecentGame = data.items[0].$ref
+    const res = await fetch("https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events", { method: "get" });
+    const data = await res.json();
+    const mostRecentGame = data.items[0].$ref;
 
-    const gameInfo = await fetch(mostRecentGame, { method: "get" })
-    var newData = await gameInfo.json()
+    const gameInfo = await fetch(mostRecentGame, { method: "get" });
+    const newData = await gameInfo.json();
 
-    const gameHeadline = newData.competitions[0].notes[0].headline
-    const gameCompetitors = newData.competitions[0].competitors
+    // this throws an error during the season; fine in the offseason
+    try {
+      var gameHeadline = newData.competitions[0].notes[0].headline;
+    }
+    catch (err) {
+        return null;
+    }
+
+    const gameCompetitors = newData.competitions[0].competitors;
 
     if (gameHeadline.startsWith("Super Bowl")) {
         for (var competitor of gameCompetitors) {
@@ -20,7 +27,7 @@ async function getSuperBowlWinner() {
         }
     }
 
-    return null
+    return null;
 }
 
-export default getSuperBowlWinner
+export default getSuperBowlWinner;
--- app/components/SelectedTeam.tsx
+++ app/components/SelectedTeam.tsx
@@ -1,22 +1,24 @@
 import 'client-only';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, createContext } from 'react';
 import Image from "next/image";
 import AFCLogo from "../images/afc.svg";
 import NFCLogo from "../images/nfc.svg";
 import Tabs from './SelectedTeamTabs/Tabs';
 import getTeam from '../apiCalls/getTeam';
-import getTeamSchedule from '../apiCalls/getTeamSchedule';
 import getSuperBowlWinner from '../apiCalls/getSuperBowlWinner';
 import allTeamsColors from "./data/allTeamsColors.json";
+import fetchCurrentSeason from '../apiCalls/fetchCurrentSeason';
+
+export const ThisSeason = createContext(null);
 
 export default function SelectedTeam({ teamID }) {
     const [team, setTeam] = useState([]);
     const [teamRecord, setTeamRecord] = useState("");
     const [teamLogo, setTeamLogo] = useState([]);
     const [teamColors, setTeamColors] = useState({});
-    //const [teamTextColor, setTeamTextColor] = useState("")
     //const [teamVenueImage, setTeamVenueImage] = useState("")
     const [teamConference, setTeamConference] = useState();
+    const [currentSeason, setCurrentSeason] = useState("");
     const [sbWinner, setSBWinner] = useState({});
 
     const afcNum = 8;
@@ -25,26 +27,13 @@ export default function SelectedTeam({ teamID }) {
         (res) => {
             setTeam(res);
             setTeamConference(res.groups.parent.id);
-            setTeamColors(allTeamsColors[res.id])
+            Object.keys(res.record).length != 0 && setTeamRecord(res.record.items[0].summary);
 
-            // the Chargers don't have an image of their venue; this statement prevents an error from being thrown for that
-            /*
-            if (res.franchise.venue.images.length > 0) {
-                setTeamVenueImage(res.franchise.venue.images[0].href)
-            }
-            else {
-                setTeamVenueImage("")
-            }
-            */
             // the color of the Giants' and Jets' default logo isn't viewable against their primary color
             res.shortDisplayName == "Giants" || "Jets" ? setTeamLogo(res.logos[1]) : setTeamLogo(res.logos[0]);
         }
     );
-
-    const getSelectedTeamRecord = () => getTeamSchedule(teamID).then(
-        (res) => setTeamRecord(res.team.recordSummary)
-    );
-
+    
     const getSBWinner = () => getSuperBowlWinner().then(
         (res) => {
             if (res) {
@@ -57,47 +46,38 @@ export default function SelectedTeam({ teamID }) {
     );
     
     useEffect(() => {
-        getSBWinner()
+        getSBWinner(),
+        fetchCurrentSeason().then(
+            res => setCurrentSeason(res)
+        );
     }, []);
    
     // run these functions every time teamID updates (aka when a new card is clicked)
     useEffect(() => {
-        getSelectedTeam(),
-        getSelectedTeamRecord()
+        setTeamColors(allTeamsColors[teamID]),
+        getSelectedTeam()
     }, [teamID]);
 
     return (
         <section className="w-full mb-24">
-            <div className="grid md:flex relative h-64 md:h-96 overflow-hidden items-center">
-                {/* 
-                teamVenueImage
-                    ? <img className="absolute w-full left-0 top-0 scale-[2] md:scale-125 origin-[50%_30%] md:origin-[50%_70%] h-auto opacity-5 mix-blend-luminosity" src={ teamVenueImage } alt={ team.displayName + " venue" } />
-                    : null
-    */}
-                {  /*<div className="absolute w-full h-full blur-md" style={{ background: 'linear-gradient(to bottom, #' + team.color + ', #' + team.alternateColor + ')' }}></div>*/ }
+            <div className="grid md:flex relative h-64 md:h-80 overflow-hidden items-center">
                 <div className="absolute w-full h-full" style={{ backgroundColor: teamColors.bgColor }}></div>
                 <div className="md:flex relative w-full justify-between items-center md:mx-6 lg:mx-14 3xl:m-auto 3xl:max-w-[1700px]">
-                    { /*
-                    <div className="flex justify-center md:block">
-                        <div className="w-40 p-1 rounded-md" 
-                        style={{ background: 'linear-gradient(to bottom right, #' + team.color + ' 0%, #' + team.color + ' 50%, #' + team.alternateColor + ' 50%, #' + team.alternateColor + ' 100%)'}}
-                        >
-                            <img src={ teamLogo.href } alt={ team.displayName + " logo" } />
-                        </div>
-                    </div>
-                    */}
                     <img className="hidden md:block w-12 sm:w-24 md:w-28 lg:w-40" src={ teamLogo.href } alt={ team.displayName + " logo" } />
                     <div className="text-center" style={{ color: teamColors.textColor }}>
                         <p className="font-protest uppercase text-3xl md:text-5xl mb-1">{ team.location }</p>
                         <p className="font-protest uppercase text-6xl md:text-8xl mb-2">{ team.name }</p>
-                        <p className="font-rubik text-sm md:text-base font-semibold flex justify-center">
-                            { teamRecord } &#183; { team.standingSummary} 
-                            { sbWinner.winner == teamID 
-                                ? <p className="font-rubik flex items-end">
+                        <p className="font-rubik text-sm md:text-base font-semibold flex gap-1.5 justify-center">
+                            { teamRecord 
+                                ? <span>{ teamRecord } &#183;</span>
+                                : <span>{ currentSeason } &#183;</span>
+                            }
+                            { team.standingSummary} 
+                            { sbWinner.winner == teamID &&
+                                <span className="font-rubik flex items-end">
                                     <span className="px-1.5">&#183;</span>
                                     <span>&#127942;</span>
-                                  </p> 
-                                : null 
+                                </span>   
                             }
                         </p>
                     </div>
@@ -116,32 +96,9 @@ export default function SelectedTeam({ teamID }) {
                 </div>
                 }
             </div>
-            { /*
-            <div className="grid md:flex gap-y-2.5 gap-x-5 items-center bg-cover bg-center" 
-            style={{ backgroundImage: 'linear-gradient(black, black), url(' + teamVenueImage + ')', backgroundBlendMode: 'saturation'}}
-            >
-                <div className="flex justify-center md:block">
-                    <div className="w-72 md:w-36 p-1 rounded-md" 
-                    style={{ background: 'linear-gradient(to bottom right, #' + team.color + ' 0%, #' + team.color + ' 50%, #' + team.alternateColor + ' 50%, #' + team.alternateColor + ' 100%)'}}
-                    >
-                        <img src={ teamLogo.href } alt={ team.displayName + " logo"} />
-                    </div>
-                </div>
-                <div className="grid md:flex gap-3 h-1/2">
-                    <h1 className="font-protest flex items-end text-5xl uppercase">{ team.displayName }</h1>
-                    <p className="font-rubik flex items-end text-secondaryGrey">
-                        { teamRecord } | { team.standingSummary} 
-                        { sbWinner.winner == teamID 
-                            ? <p className="font-rubik flex items-end text-gold">
-                                <span className="text-secondaryGrey px-1.5">|</span> { sbWinner.headline } Champions
-                              </p> 
-                            : null 
-                        }
-                    </p>
-                </div>          
-            </div>
-            */ }
-            <Tabs teamID={ teamID } />
+            <ThisSeason.Provider value={ currentSeason }>
+                <Tabs teamID={ teamID } />    
+            </ThisSeason.Provider>
         </section>
     )
 }
