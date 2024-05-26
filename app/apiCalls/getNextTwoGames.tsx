6526a3315a5e0f07f00ac071b9919bed6ef5e956
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-05-26 16:28:33 -0400

    add NextTwoGames

---
+++ app/apiCalls/getNextTwoGames.tsx
@@ -0,0 +1,50 @@
+import displayWeek from "../components/helpers/displayWeekInfo";
+
+export default async function getNextTwoGames({ teamID }) {
+    let nextTwoGames = [];
+    
+    // fetch the post, regular, and preseason schedules for the chosen team
+    for(let counter = 1; counter <= 3; counter += 1) {
+        const res1 = await fetch(`https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/${teamID}/schedule?seasontype=${counter}`, { method: "GET" });
+        const schedule = await res1.json();   
+        
+        if (schedule.requestedSeason) {
+            for (const game of schedule.events) {
+                if (nextTwoGames.length == 2) return nextTwoGames;
+
+                let data = {
+                    id: game.id, // used in JSX as a key
+                    date: game.date,
+                    teams: game.competitions[0].competitors,
+                    status: game.competitions[0].status,
+                    week: displayWeek(schedule.requestedSeason.name, schedule.events, game),
+                    //season: game.season.year,
+                    seasonType: game.seasonType
+                }
+
+                if (game.competitions[0].status.type.state == "pre") {
+                    data.network = game.competitions[0].broadcasts.length > 0 ? game.competitions[0].broadcasts[0].media.shortName : "TBD";
+                    
+                    // an error will be thrown if the spread/odds haven't been added yet
+                    try {
+                        const fetchGameOdds = await fetch(`https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/${game.id}/competitions/${game.id}/odds/40`, { method: "GET" });
+                        const gameOdds = await fetchGameOdds.json(); 
+              
+                        if (gameOdds.error) throw new Error();
+          
+                        data.spread = gameOdds.details;
+                        data.overUnder = gameOdds.overUnder;
+                    }
+                    catch (err) {
+                        data.spread= "TBD"; 
+                        data.overUnder = "TBD";
+                    }
+                    
+                    nextTwoGames.push(data);
+                }
+            }
+        }
+    }
+
+    return nextTwoGames;
+}
--- app/components/SelectedTeamTabs/OverviewComponents/Overview.tsx
+++ app/components/SelectedTeamTabs/OverviewComponents/Overview.tsx
@@ -1,32 +1,14 @@
 import 'client-only';
-import { useState, useEffect } from 'react';
-import fetchCurrentSeason from "@/app/apiCalls/fetchCurrentSeason";
-//import getTeam from '@/app/apiCalls/getTeam';
+import { useContext } from 'react';
 import TeamLeaders from './TeamLeaders';
 import TeamRankings from './TeamRankings';
 import LastTwoGames from './LastTwoGames';
+import NextTwoGames from './NextTwoGames';
+import { ThisSeason } from '@/app/page';
 
 export default function Overview({ teamID }) {
-    const [currentSeason, setCurrentSeason] = useState("")
-    //const [teamAltColor, setTeamAltColor] = useState("")
+    const currentSeason = useContext(ThisSeason);
 
-    const getCurrentSeason = () => fetchCurrentSeason().then(
-        (res) => setCurrentSeason(res)
-    )
-    /*
-    const getTeamAltColor = () => getTeam({teamID}).then(
-        (res) => setTeamAltColor(res.alternateColor)
-    )
-    */
-    // only run getCurrentSeason() on the first render
-    useEffect(() => {
-        getCurrentSeason()
-    }, []);
-    /*
-    useEffect(() => {
-        getTeamAltColor()
-    }, [teamID])
-    */
     return (
         <>
             <h2 className="font-protest text-3xl 2xl:text-4xl uppercase pb-2 mb-9 border-b-2">{ currentSeason } overview</h2>
@@ -34,12 +16,7 @@ export default function Overview({ teamID }) {
             <TeamRankings teamID={ teamID } />
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
                 <LastTwoGames teamID={ teamID }/>
-                <div>
-                    <h3 className="font-protest text-2xl 2xl:text-3xl pb-2">Next Two Games</h3>
-                    <div className="font-rubik grid grid-cols-2 gap-3 bg-sectionColor p-3 rounded-md">
-
-                    </div>
-                </div>
+                <NextTwoGames teamID={ teamID } />
             </div>
         </>
     )
