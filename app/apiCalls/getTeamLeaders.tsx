c805c403b709797b5cfb292868f1baf5652004ab
dimitrikjohnson <dimitrik.johnson@gmail.com>
2024-05-26 16:22:17 -0400

    more code cleanup

--- app/apiCalls/getTeamLeaders.tsx
+++ app/apiCalls/getTeamLeaders.tsx
@@ -1,24 +1,28 @@
 import fetchCurrentSeason from "./fetchCurrentSeason";
 
 export default async function getTeamLeaders( teamID, responseType ) {
-    const currentSeason = await fetchCurrentSeason()
+    const currentSeason = await fetchCurrentSeason();
 
-    const res = await fetch("https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/" + currentSeason + "/types/2/teams/" + teamID + "/leaders", { method: "get" })
-    const data = await res.json()
+    const res = await fetch(`https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/${currentSeason}/types/2/teams/${teamID}/leaders`, { method: "get" });
+    const data = await res.json();
 
-    if (!res.ok) {
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error('Failed to fetch team leaders')
+    // This will activate the closest `error.js` Error Boundary
+    if (!res.ok) throw new Error('Failed to fetch team leaders');
+
+    let categories = [data.categories[3], data.categories[4], data.categories[5], data.categories[7]];
+    
+    // if the response type is "detailed" send a few more stat leaders; otherwise, only send the main ones
+    if (responseType == "detailed") {
+        categories.push(data.categories[8], data.categories[6]);
+        categories.splice(2, 0, data.categories[11]);
+        categories.splice(4, 0, data.categories[13]);
     }
 
-    var categories
-    // if the response type is "detailed" send more stat leaders; otherwise, only send the main ones
-    responseType == "detailed" ? categories = data.categories : categories = [data.categories[3], data.categories[4], data.categories[5], data.categories[7]]
-    const output = []
+    const output = [];
 
     for (const category of categories) {
-        const athleteRes = await fetch(category.leaders[0].athlete.$ref, { method: "get" })
-        const athleteData = await athleteRes.json()
+        const athleteRes = await fetch(category.leaders[0].athlete.$ref, { method: "get" });
+        const athleteData = await athleteRes.json();
 
         output.push({
             statName: category.displayName,
@@ -27,10 +31,8 @@ export default async function getTeamLeaders( teamID, responseType ) {
             playerHeadshot: athleteData.headshot.href,
             playerJersey: athleteData.jersey,
             playerPosition: athleteData.position.abbreviation
-        })
+        });
     }
 
-    return output
+    return output;
 }
-
-//export default getTeamLeaders
--- app/components/SelectedTeamTabs/OverviewComponents/TeamLeaders.tsx
+++ app/components/SelectedTeamTabs/OverviewComponents/TeamLeaders.tsx
@@ -14,23 +14,36 @@ export default function TeamLeaders({ teamID, responseType }) {
         // this function accounts for the possibility of a missing number
         const isJerseyNumPresent = (num) => {
             if (num) {
-                return <span>&#183; #{ num }</span>
+                return (
+                    <>
+                        <span>&#183;</span> 
+                        <span>#{ num }</span>
+                    </> 
+                )
             }
         }
 
-        return (<>
-            { teamLeaders.map(stat =>
-                <div key={ stat.statName + stat.playerName } className="bg-sectionColor grid justify-center justify-items-center text-center pt-3 px-3 pb-0 rounded-md">
-                    <p className="mb-2 uppercase">{ stat.statName }</p>
-                    <p className="mb-2 font-semibold text-lg">{ stat.statValue }</p>
-                    <p className="mb-1">{ stat.playerName }</p>
-                    <p className="text-sm text-lighterSecondaryGrey mb-2">{ stat.playerPosition } { isJerseyNumPresent(stat.playerJersey) }</p>
-                    <div className="w-32 md:w-40 rounded-sm shrink-0">
-                        <img src={ stat.playerHeadshot } alt={ stat.playerName } />
+        return (
+            <>
+                { teamLeaders.map(stat =>
+                    <div 
+                        key={ stat.statName + stat.playerName } 
+                        className="bg-sectionColor grid justify-center justify-items-center text-center pt-3 px-3 pb-0 rounded-md"
+                    >
+                        <p className="mb-2 uppercase">{ stat.statName }</p>
+                        <p className="mb-2 font-semibold text-lg">{ stat.statValue }</p>
+                        <p className="mb-1">{ stat.playerName }</p>
+                        <p className="flex gap-1 text-sm text-lighterSecondaryGrey mb-2">
+                            <span>{ stat.playerPosition }</span> 
+                            <span className="flex gap-1">{ isJerseyNumPresent(stat.playerJersey) }</span>
+                        </p>
+                        <div className="w-32 md:w-40 rounded-sm shrink-0">
+                            <img src={ stat.playerHeadshot } alt={ stat.playerName } />
+                        </div>
                     </div>
-                </div>
-            )}
-        </>)
+                )}
+            </>
+        )
     }
 
     useEffect(() => {
