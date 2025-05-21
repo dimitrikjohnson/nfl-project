098fbb614231257f94d355fff4dcc14cdcb4d8b0
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-05-21 19:30:00 -0400

    distinguish between team and league leaders

--- app/formatAPIcalls/formatLeaders.tsx
+++ app/formatAPIcalls/formatLeaders.tsx
@@ -1,4 +1,4 @@
-export default async function formatLeaders(season, data, getLeadersOverview, getLeagueLeaders = false) {
+export default async function formatLeaders(season, seasonType, data, getLeadersOverview, getLeagueLeaders = false) {
     let categories;
 
     // the categories that will be displayed for team leaders vs. league leaders are different
@@ -21,15 +21,22 @@ export default async function formatLeaders(season, data, getLeadersOverview, ge
     }
 
     // season is for keeping track of the season that is being displayed
-    const output = [season, []];
+    const output = [season, seasonType, getLeagueLeaders ? {} : []];
 
     for (const category of categories) {
         /*
          * an error is thrown if there is no leader for a category
          * this try/catch skips the problematic category entirely
-         * example: no one on the team has recoreded an interception yet
+         * example: no one on the team has recorded an interception yet
         */
         try {
+            if (getLeagueLeaders) {
+                output[2][category.displayName] = await leagueLeaders(category);
+            }
+            else {
+                output[2].push(await teamLeaders(category));
+            }
+            /*
             const athleteRes = await fetch(category.leaders[0].athlete.$ref, { method: "get" });
             if (!athleteRes.ok) throw new Error('Something went wrong');
             
@@ -41,10 +48,12 @@ export default async function formatLeaders(season, data, getLeadersOverview, ge
                 const athleteTeamRes = await fetch(category.leaders[0].team.$ref, { method: "get" });
                 athleteTeam = await athleteTeamRes.json();
             }
-
-            output[1].push({
+            
+            // QB Rating needs displayValue, everything else can use value
+            // toLocaleString adds a comma to numbers in the thousands
+            output[2].push({
                 statName: category.displayName,
-                statValue: category.leaders[0].displayValue,
+                statValue: category.shortDisplayName == "RAT" ? category.leaders[0].displayValue : (category.leaders[0].value).toLocaleString('en-US'), 
                 playerName: athleteData.displayName,
                 playerHeadshot: athleteData.headshot.href,
                 playerJersey: athleteData.jersey,
@@ -53,10 +62,58 @@ export default async function formatLeaders(season, data, getLeadersOverview, ge
                 playerTeamAbbreviation: athleteTeam?.abbreviation,
                 playerTeamName: athleteTeam?.displayName,
                 playerTeamLogo: athleteTeam?.logos[0].href
-            });    
+            });
+            */ 
         }
         catch (error) { continue; }
     }
-
+    //console.log(output)
     return output;
+}
+
+async function teamLeaders(category) {
+    const athleteRes = await fetch(category.leaders[0].athlete.$ref, { method: "get" });
+    if (!athleteRes.ok) throw new Error('Something went wrong');
+            
+    const athleteData = await athleteRes.json();
+            
+    // QB Rating needs displayValue, everything else can use value
+    // toLocaleString adds a comma to numbers in the thousands
+    return {
+        statName: category.displayName,
+        statValue: category.shortDisplayName == "RAT" ? category.leaders[0].displayValue : (category.leaders[0].value).toLocaleString('en-US'), 
+        playerName: athleteData.displayName,
+        playerHeadshot: athleteData.headshot.href,
+        playerJersey: athleteData.jersey,
+        playerPosition: athleteData.position.abbreviation
+    }     
+}
+
+async function leagueLeaders(category) {
+    let leaders = [];
+    
+    for (let count = 0; count < 3; count += 1) {
+        const athleteRes = await fetch(category.leaders[count].athlete.$ref, { method: "get" });
+        if (!athleteRes.ok) throw new Error('Something went wrong');
+            
+        const athleteData = await athleteRes.json();
+      
+        const athleteTeamRes = await fetch(category.leaders[count].team.$ref, { method: "get" });
+        let athleteTeam = await athleteTeamRes.json();
+            
+        // QB Rating needs displayValue, everything else can use value
+        // toLocaleString adds a comma to numbers in the thousands
+        leaders.push({
+            statValue: category.shortDisplayName == "RAT" ? category.leaders[count].displayValue : (category.leaders[count].value).toLocaleString('en-US'), 
+            playerName: athleteData.displayName,
+            playerHeadshot: athleteData.headshot.href,
+            playerJersey: athleteData.jersey,
+            playerPosition: athleteData.position.abbreviation,
+            playerTeamID: athleteTeam.id,
+            playerTeamAbbreviation: athleteTeam.abbreviation,
+            playerTeamName: athleteTeam.displayName,
+            playerTeamLogo: athleteTeam.logos[0].href
+        });
+    }   
+    return leaders
 }
