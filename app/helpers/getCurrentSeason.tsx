19b1175258ddd93841cf47c4796e7d26eb0ee609
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-05-21 19:32:32 -0400

    return season type

--- app/helpers/getCurrentSeason.tsx
+++ app/helpers/getCurrentSeason.tsx
@@ -3,8 +3,12 @@ import { cache } from 'react';
 
 const getCurrentSeason = cache(async () => {
     const res = await fetch("https://sports.core.api.espn.com/v2/sports/football/leagues/nfl", { method: "get" });
-    const data = await res.json()
-    return data.season.year
+    const data = await res.json();
+    
+    return {
+        year: data.season.year,
+        type: data.season.type.type
+    }
 });
 
 export default getCurrentSeason;
