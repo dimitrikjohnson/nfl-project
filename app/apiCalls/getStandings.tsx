92ad538e7185769112f2a9d8cc49a8d861e3f675
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-06-13 19:42:04 -0400

    add https to fetch

--- app/apiCalls/getStandings.tsx
+++ app/apiCalls/getStandings.tsx
@@ -11,13 +11,13 @@ export default async function getStandings(groupNum: string) {
     // the only time there should be an error is during a small window in the offseason when the season number changes in the API response
     let res;
     try {
-        res = await fetch(`http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/${currentSeason}/types/2/groups/${groupNum}/standings/0`, { 
+        res = await fetch(`https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/${currentSeason}/types/2/groups/${groupNum}/standings/0`, { 
             method: "get" 
         });    
         if (!res.ok) throw new Error("Something went wrong");
     }
     catch (error) {
-        res = await fetch(`http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/${currentSeason-1}/types/2/groups/${groupNum}/standings/0`, { 
+        res = await fetch(`https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/${currentSeason-1}/types/2/groups/${groupNum}/standings/0`, { 
             method: "get" 
         });
         displayedSeason = currentSeason-1;
