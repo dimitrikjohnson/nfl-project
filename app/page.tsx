31a9123dd06540870a3c5bfc068e810957fefd5d
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-06-25 19:44:47 -0400

    revalidate homepage every 60 seconds

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS manually
 config.autoAddCss = false;
 
+export const revalidate = 60; // revalidates the page after 60 seconds (needed for getting a new background image)
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col">
