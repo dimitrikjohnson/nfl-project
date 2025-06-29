8f601b7358a89502ac43be6b63dab11b1e10ddff
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-06-28 21:54:13 -0400

    create components for H2 and H3 elements

---
+++ app/components/H2.tsx
@@ -0,0 +1,5 @@
+export default function H2({ children }: { children: React.ReactNode }) {
+    return (
+        <h2 className="font-protest text-3xl lg:text-4xl uppercase">{ children }</h2>
+    )
+}
---
+++ app/components/H3.tsx
@@ -0,0 +1,5 @@
+export default function H3({ children }: { children: React.ReactNode }) {
+    return (
+        <h3 className="font-protest text-2xl lg:text-3xl pb-3">{ children }</h3>
+    )
+}
