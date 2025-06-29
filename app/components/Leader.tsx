3928b8cf711aa4342d9935fa551db79d6ce77e09
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-06-29 12:06:38 -0400

    improve Leader grid layout sizing

--- app/components/Leader.tsx
+++ app/components/Leader.tsx
@@ -12,8 +12,8 @@ export default function Leader({ stat }: { stat: PlayerStatLeader }) {
     
     return (
         <div className="w-full bg-section border border-gray-300 dark:bg-section-dark dark:border-none grid justify-center justify-items-center text-center pt-3 px-3 pb-0 rounded-md">
-            <p className="hidden md:block mb-2 uppercase">{ stat.statName }</p>
-            <p className="md:hidden mb-2 uppercase">{ tdMobile }</p>
+            <p className="hidden lg:block mb-2 uppercase">{ stat.statName }</p>
+            <p className="lg:hidden mb-2 uppercase">{ tdMobile }</p>
             <p className="mb-2 font-semibold text-lg">{ stat.statValue }</p>
             <p className="mb-1">{ stat.playerName }</p>
             <p className="mb-2 flex gap-1 text-sm text-gray-500 dark:text-lighterSecondaryGrey">
