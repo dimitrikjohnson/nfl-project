be072407ebb64329fc1fff85d81e104c0e0fbc5d
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-08-26 20:43:52 -0400

    sort the games on the scoreboard by status

--- app/formatAPIcalls/formatScoreboard.tsx
+++ app/formatAPIcalls/formatScoreboard.tsx
@@ -1,3 +1,5 @@
+import { GameData } from "@/app/types/schedule";
+
 export default async function formatScoreboard() {
     let res = await fetch('https://cdn.espn.com/core/nfl/scoreboard?xhr=1&limit=50', { method: "GET" });
     let fullRes = await res.json();
@@ -6,7 +8,9 @@ export default async function formatScoreboard() {
     const dateParams = fullRes.content.dateParams;
 
     // get the current week of the season; if playoffs, get the round name
-    const weekNum = dateParams.seasontype == 3 ? fullRes.content.calendar[2].entries[dateParams.week-1].label : dateParams.week;
+    const weekNum = dateParams.seasontype == 3 
+        ? fullRes.content.calendar[2].entries[dateParams.week-1].label 
+        : dateParams.week;
 
     // get the date range for the current week
     const weekDetail = fullRes.content.calendar[dateParams.seasontype-1].entries[dateParams.week-1].detail;
@@ -15,7 +19,9 @@ export default async function formatScoreboard() {
 
     for (const game of data.events) {
         const status = game.status;
-        const gameStatus = status.type.description == "In Progress" ? `Q${status.period} ${status.displayClock}` : status.type.shortDetail;
+        const gameStatus = status.type.description == "In Progress" 
+            ? `Q${status.period} ${status.displayClock}` 
+            : status.type.shortDetail;
 
         const comp = game.competitions[0];
 
@@ -29,11 +35,22 @@ export default async function formatScoreboard() {
             //downDistance: status.type.description == "In Progress" && comp.situation.shortDownDistanceText,
             //yardLine: status.type.description == "In Progress" && comp.situation.possessionText,
             possession: status.type.description == "In Progress" && comp.situation.possession,
-            //awayTeam: comp.competitors[1],
-            //homeTeam: comp.competitors[0],
             teams: [comp.competitors[1], comp.competitors[0]]
         });
     }
+
+    // Sort: in progress (state == "in") first, scheduled (state == "pre") next, completed last (state == "post")
+    output[2].sort((a: GameData, b: GameData) => {
+        const order = {
+            in: 0,   // in-progress games first
+            pre: 1,  // scheduled games second
+            post: 2  // completed games last
+        };
+
+        if (a.state && b.state) {
+            return order[a.state] - order[b.state];    
+        }
+    });
 
     return output;
 }
