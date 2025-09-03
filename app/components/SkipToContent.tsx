4e13b811c39a34c9713b762310ddcc2a44358131
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-09-03 19:57:31 -0400

    visually hide the link on mobile

--- app/components/SkipToContent.tsx
+++ app/components/SkipToContent.tsx
@@ -1,45 +1,10 @@
 'use client';
 import Link from "next/link";
-import { useEffect } from "react";
 
 export default function SkipToContent() {
-    useEffect(() => {
-        const handleFirstTab = (e: KeyboardEvent) => {
-            if (e.key === "Tab") {
-                document.body.classList.add("user-is-tabbing");
-
-                window.removeEventListener("keydown", handleFirstTab);
-                window.addEventListener("mousedown", handleMouseDownOnce);
-                window.addEventListener("touchstart", handleMouseDownOnce);
-            }
-        };
-
-        const handleMouseDownOnce = () => {
-            document.body.classList.remove("user-is-tabbing");
-
-            window.addEventListener("keydown", handleFirstTab);
-            window.removeEventListener("mousedown", handleMouseDownOnce);
-            window.removeEventListener("touchstart", handleMouseDownOnce);
-        };
-
-        window.addEventListener("keydown", handleFirstTab);
-        return () => {
-            window.removeEventListener("keydown", handleFirstTab);
-            window.removeEventListener("mousedown", handleMouseDownOnce);
-            window.removeEventListener("touchstart", handleMouseDownOnce);
-        };
-    }, []);
-
     return (
         <Link 
-			className="absolute left-0 top-0 block z-50
-                -translate-x-full
-                rounded-md bg-cyan-400 text-backdrop-dark
-                px-4 py-3 text-sm font-bold uppercase tracking-wide
-
-                focus-visible:translate-x-0
-                [body.user-is-tabbing_&]:-translate-x-full
-            "
+			className="absolute left-0 top-0 block z-50 -translate-x-full rounded-md bg-cyan-400 text-backdrop-dark px-4 py-3 text-sm font-bold uppercase tracking-wide md:focus-visible:translate-x-0"
 			href="#content">
 				Skip to content
 		</Link>
