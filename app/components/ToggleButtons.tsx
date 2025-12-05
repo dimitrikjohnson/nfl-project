15e9daa99cba2ed24d6d8548ac608b48f24f95b6
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-12-05 01:37:37 -0500

    redesign toggle buttons

--- app/components/ToggleButtons.tsx
+++ app/components/ToggleButtons.tsx
@@ -8,22 +8,23 @@ type ToggleProps<T extends string> = {
     onChange: (value: T) => void;
     renderLabel?: (value: T) => React.ReactNode; 
     uppercase?: boolean; // optional helper for styling consistency
+    fullWidth?: boolean; // optional helper for buttons that need to be full width on mobile
 }
 
 // toggle buttons for switching between given options
-export default function ToggleButtons<T extends string>({ options, extraClasses="", valueToMatch, onChange, renderLabel, uppercase }: ToggleProps<T>) {
+export default function ToggleButtons<T extends string>({ options, extraClasses="", valueToMatch, onChange, renderLabel, uppercase, fullWidth }: ToggleProps<T>) {
     return (
-        <div className={`gap-2.5 md:gap-3 ${extraClasses}`}>
+        <div className={`border border-gray-600/50 dark:border-lighterSecondaryGrey/50 rounded-md overflow-hidden ${extraClasses}`}>
             { options.map(option => {
                 const isActive = valueToMatch === option;
                 return (
                     <button 
                         key={ option } 
-                        className={`btn h-8 min-h-8 px-3.5 md:px-4 md:h-10 md:min-h-10 dark:border-none ${uppercase ? "uppercase": ""} ${ isActive 
+                        className={`btn rounded-none h-9 min-h-9 px-3 md:px-4 md:h-10 md:min-h-10 ${ uppercase ? "uppercase": "capitalize" } 
+                            ${ fullWidth ? "w-1/2 md:w-auto" : "" } ${ isActive 
                             ? "bg-primary text-primary-dark hover:bg-primary hover:text-primary-dark border-none \
                                 dark:bg-primary-dark dark:text-backdrop-dark dark:hover:bg-primary-dark dark:hover:text-backdrop-dark" 
-                            : "bg-alt-table-row text-primary border border-gray-300 hover:bg-secondaryGrey/[0.25] \
-                                dark:border-none dark:bg-alt-table-row-dark dark:text-primary-dark" 
+                            : "bg-transparent font-normal text-primary hover:bg-secondaryGrey/[0.25] dark:text-primary-dark" 
                         }`}
                         onClick={ () => onChange(option) }
                     >
