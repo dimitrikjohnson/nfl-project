d7bec23a5fb70d8a844acabb5dbc9dc1849eb1f9
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-12-24 15:59:33 -0500

    add draft types

--- app/types/albinoskies.ts
+++ app/types/albinoskies.ts
@@ -25,3 +25,48 @@ export type User = {
 export type Users = {
     [userID: string]: User
 }
+
+// DRAFTS
+
+export type Draft = {                  // single draft
+    draftID: string;
+    draftOrder: DraftOrder;
+    draftType: "snake" | "linear";
+    leagueID: string;
+    season: string;
+    users: DraftUser[];
+    picks: DraftPick[];
+    
+}
+
+type DraftMap = Record<string, Draft>;  // collection of drafts
+
+type DraftUser = {
+    userID: string;
+    displayName: string;
+    avatar: string;
+    realName: string;
+}
+
+export type DraftPick = {
+    pickNumber: number;
+    round: number;
+    userID: string;
+    player: {
+        firstName: string;
+        headshot: string;
+        id: string;
+        lastName: string;
+        link: string;
+        position: string;
+        team: string;
+    } | null;
+    defense: {
+        link: string;
+        logo: string;
+        name: string;
+        team: string;
+    } | null;
+}
+
+export type DraftOrder = string[];
