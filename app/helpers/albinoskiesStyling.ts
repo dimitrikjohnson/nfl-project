b03adb72b3af34da6192908868dd5e3cb2e8e5fb
dimitrikjohnson <dimitrik.johnson@gmail.com>
2025-12-24 16:00:36 -0500

    create margin layouts

---
+++ app/helpers/albinoskiesStyling.ts
@@ -0,0 +1,5 @@
+// margins for Albino Skies pages
+
+export const tableMargins = "md:mx-6 lg:mx-14 xl:mx-auto max-w-screen-xl";
+export const margins = `mx-4 ${tableMargins}`;
+export const h3Styling = `font-protest text-2xl lg:text-3xl pb-3 ${margins}`;
